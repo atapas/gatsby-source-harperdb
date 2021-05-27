8a88c014bb3df6e4866d7a79fe574796ce309f64
Tapas Adhikary <tapas.adhikary@gmail.com>
2021-05-27 13:32:30 +0530

    feat: We have the payload in the schema now. We can pass both SQL, non-SQL queries with joins

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -1,28 +1,15 @@
 
-var axios = require('axios');
+let axios = require('axios');
 exports.sourceNodes = async (
   { actions, createNodeId, createContentDigest },
   options
 ) => {
   const { createNode } = actions;
-  const { secret, operation, schema, table, url, size } = options;
+  const { secret, type, url, payload } = options;
 
   try {
-    
-    console.log('createNode is', createNode);
-    console.log('secret is', secret);
-    console.log('operation is', operation);
-    console.log('schema is', schema);
-    console.log('table is', table);
-    console.log('url is', url);
-    console.log('size is', size);
-
-    var data = JSON.stringify({
-        "operation": `${operation}`,
-        "sql":`SELECT * FROM ${schema}.${table}`
-    });
-    
-    var config = {
+    let data = JSON.stringify(payload);
+    let config = {
       method: 'post',
       url: `${url}`,
       headers: { 
@@ -34,18 +21,15 @@ exports.sourceNodes = async (
 
     let res = await axios(config);
     let documents = res.data;
-    console.log('data from harperdb', documents.length);
 
     documents.forEach(document => {
-      console.log('document', document);
-
       createNode({
         ...document,
         id: createNodeId(`harperdb-${document.id}`),
         parent: null,
         children: [],
         internal: {
-          type: `${table}`,
+          type: `${type}`,
           content: JSON.stringify(document),
           contentDigest: createContentDigest(document)
         }
